88125f9d3be8a8583ed41bdcd2b6b53221b965bc
Andrew Wilson <andrew@lightsinthesky.io>
2014-12-12 09:50:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [react-skeleton](http://lightsinthesky.github.io/react-skeleton/)
+
+react-skeleton is a React UI framework implementing the [Skeleton](http://getskeleton.com) responsive css boilerplate. 
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	Container 	: require('./lib/components/Container'),
+	Section 	: require('./lib/components/Section'),
+	Row 		: require('./lib/components/Row'),
+	Column 		: require('./lib/components/Column'),
+	Typography	: require('./lib/components/Typography'),
+	Link		: require('./lib/components/Link'),
+	Code		: require('./lib/components/Code'),
+	Table		: require('./lib/components/Table'),
+	Form		: require('./lib/components/Form'),
+	Image		: require('./lib/components/Image'),
+	List		: require('./lib/components/List').List,
+	Button		: require('./lib/components/Button')
+};
